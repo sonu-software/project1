079775cc482e577104283c8bbe7cbdc6baf9f235
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-07-31 17:26:09 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -9,33 +9,12 @@ from PIL import Image
 command1 = 'powershell -command "Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds; $bitmap = New-Object System.Drawing.Bitmap $bounds.Width, $bounds.Height; $graphics = [System.Drawing.Graphics]::FromImage($bitmap); $graphics.CopyFromScreen($bounds.Location, [System.Drawing.Point]::Empty, $bounds.Size); $bitmap.Save(\'screenshot.png\'); $graphics.Dispose(); $bitmap.Dispose();"'
 
 st.title("cmd run on web")
-
-def take_screenshot():
-    #os.system(command1)
-    file1=os.system("python take_screenshot.py")
-
-    time.sleep(5) 
-    st.success("done")
-    st.image("file1", caption="Here is the PNG image", use_container_width=True)
-
-if st.button("Take Screenshot"):
-    os.system(command1)  # Make sure this script saves screenshot.png
-    time.sleep(5)  # Wait for the screenshot to be saved
-    if os.path.exists("screenshot.png"):
-        st.success("Screenshot taken!")
-        try:
-            img = Image.open("screenshot.png")
-            st.image(img, caption="Here is the PNG image", use_container_width=True)
-        except Exception as e:
-            st.error(f"Failed to load image: {e}")
-    else:
-        st.error("Screenshot failed. File not found.")
-
-
-if st.button("press this"):
+def security_code():
     os.system("netsh wlan show all > output2.txt")
-    
     time.sleep(5)
+    with open("output.txt","r") as file:
+        content=file.read()
+        st.write(content)
 
 if __name__=="__main__":
     take_screenshot()
