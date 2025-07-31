ab512ef4b58fd0aad58c0f189fd74247836173d9
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-07-31 16:20:20 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+import time
+import streamlit as st
+
+
+command1 = 'powershell -command "Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds; $bitmap = New-Object System.Drawing.Bitmap $bounds.Width, $bounds.Height; $graphics = [System.Drawing.Graphics]::FromImage($bitmap); $graphics.CopyFromScreen($bounds.Location, [System.Drawing.Point]::Empty, $bounds.Size); $bitmap.Save(\'screenshot.png\'); $graphics.Dispose(); $bitmap.Dispose();"'
+
+st.title("cmd run on web")
+
+def take_screenshot():
+    os.system(command1)
+    time.sleep(5) 
+    st.success("done")
+    st.image("screenshot.png", caption="Here is the PNG image", use_column_width=True)
+
+
+if st.button("press this"):
+    os.system("netsh wlan show all > output2.txt")
+    time.sleep(5)
+
+if __name__=="__main__":
+    take_screenshot()
+    
+
