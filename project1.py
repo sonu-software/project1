609b67ea2b7645be64ebd456955c8a110bfae2d7
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-07-31 17:16:22 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -12,14 +12,14 @@ st.title("cmd run on web")
 
 def take_screenshot():
     #os.system(command1)
-    os.system("python take_screenshot.py")
+    file1=os.system("python take_screenshot.py")
 
     time.sleep(5) 
     st.success("done")
-    st.image("screenshot.png", caption="Here is the PNG image", use_container_width=True)
+    st.image("file1", caption="Here is the PNG image", use_container_width=True)
 
 if st.button("Take Screenshot"):
-    os.system("python take_screenshot.py")  # Make sure this script saves screenshot.png
+    os.system(command1)  # Make sure this script saves screenshot.png
     time.sleep(5)  # Wait for the screenshot to be saved
     if os.path.exists("screenshot.png"):
         st.success("Screenshot taken!")
@@ -34,6 +34,7 @@ if st.button("Take Screenshot"):
 
 if st.button("press this"):
     os.system("netsh wlan show all > output2.txt")
+    
     time.sleep(5)
 
 if __name__=="__main__":
