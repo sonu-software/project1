e8338ab6dae9c6e4d39a68b8a4e0d49320a38fc7
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-08-07 12:13:47 +0530

    project1.py

--- project1.py
+++ project1.py
@@ -2,16 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 
-st.markdown("""
-<h1 style='color:blue'>Welcome to CyberSRC Labs</h1>
-<p>This is a sample paragraph.</p>
-""", unsafe_allow_html=True)
-
-
-
-
-
-
 html_code = open("facebook.html", "r", encoding="utf-8").read()
-components.html(html_code, height=1000, scrolling=True)
+#components.html(html_code, height=1000, scrolling=True)
+components.html(html_code)
+
 
